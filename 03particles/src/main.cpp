0d2e24c46b535bce41e96ceaa125660ce6582447
Tadeusz <pantadeusz@pjwstk.edu.pl>
2017-12-21 11:22:08 +0100

    przyklad z wykladu - na zywo

--- 03particles/src/main.cpp
+++ 03particles/src/main.cpp
@@ -8,6 +8,11 @@
 #include "vid_sdl.hpp"
 
 #include <chrono>
+#include <thread>
+#include <vector>
+#include <algorithm>
+#include <random>
+
 
 #include <iostream>
 
@@ -35,17 +40,35 @@ public:
 	position_t velocity;
 	position_t accel;
 
-	Particle( const position_t &p0, const position_t &v0 = {0, 0}, const position_t &a0 = {0, 0} ): position( p0 ), velocity(v0), accel(a0) {
+	double ttl; // czas zycia
 
+	Particle( const position_t &p0, const position_t &v0 = {0, 0}, const position_t &a0 = {0, 0} ): position( p0 ), velocity( v0 ), accel( a0 ) {
+		ttl = 1;
 	}
 
 	void update( std::chrono::duration<double> &dt ) {
 		double dts = dt.count();
 		position = position + velocity * dts + accel * dts * dts * 0.5;
 		velocity = velocity + accel * dts;
+		ttl -= dts;
 	}
 
 };
+
+void calculateParticles( std::vector < Particle >  &particles0, std::chrono::duration<double> &dt ) {
+	std::vector < bool> dels( particles0.size() );
+	int i = 0;
+	auto particles = particles0;
+	for ( auto & p : particles ) {
+		p.update( dt );
+		dels[i] = p.ttl < 0;
+	}
+	particles0.clear();
+	for ( int i = 0; i < particles.size(); i++ ) {
+		if ( !dels[i] ) particles0.push_back( particles[i] );
+	}
+}
+
 
 using namespace sgd;
 
@@ -62,7 +85,8 @@ int main( ) { // int argc, char **argv ) {
 	std::unique_ptr < Uint32 > pixels ( new Uint32[16 * 16] );
 	for ( int x = 0; x < 16; x++ ) {
 		for ( int y = 0; y < 16; y++ ) {
-			pixels.get()[y * 16 + x] = ( 0x0550000 + ( int )( sin( x / 3.0 ) * 127 + 128 ) +  ( ( int )( cos( y / 3.0 ) * 127 + 128 ) << 8 ) ) & 0x0ffffff;
+//			pixels.get()[y * 16 + x] = ( 0x0550000 + ( int )( sin( x / 3.0 ) * 127 + 128 ) +  ( ( int )( cos( y / 3.0 ) * 127 + 128 ) << 8 ) ) & 0x0ffffff;
+			pixels.get()[y * 16 + x] = ( ( int )( sin( x / 3.0 + y / 6 ) * 127 + 128 ) + ( ( int )( sin( x / 3.0 + y / 6 ) * 127 + 128 ) << 8 )  + ( ( int )( sin( x / 3.0 + y / 6 ) * 127 + 128 ) << 16 ) ) & 0x0ffffff;
 			double r = std::sqrt( ( x - 8 ) * ( x - 8 ) + ( y - 8 ) * ( y - 8 ) );
 			int a = 0;
 			if ( r <= 8 ) a = 255 * ( 8 - a );
@@ -73,13 +97,21 @@ int main( ) { // int argc, char **argv ) {
 
 	std::chrono::duration<double> dt( 0.01 ); // w sekundach
 
-	SDL_Rect destRectForFace = {
-		.x = 160,
-		.y = 160,
-		.w = 16,
-		.h = 16
-	};
+	std::vector < Particle > particles;
+	particles.reserve( 30000 );
+
+
 	bool updating = false;
+	auto prevTime = std::chrono::high_resolution_clock::now(); // w sekundach
+	position_t scaleFactor = {10, 10};
+	double timeToShoot = 0;
+	double timeToSpawnParicle = 0.6;
+
+    std::random_device randomDev;
+    // Choose a random mean between 1 and 6
+    std::default_random_engine randomEngine(randomDev());
+    std::uniform_int_distribution<int> uniform_dist_x(0, 640);
+    std::uniform_int_distribution<int> uniform_dist_y(0, 480);
 	for ( bool game_active = true ; game_active; ) {
 		SDL_Event event;
 		while ( SDL_PollEvent( &event ) ) {
@@ -97,9 +129,13 @@ int main( ) { // int argc, char **argv ) {
 				break;
 			case SDL_MOUSEMOTION:
 				std::cout << "motion: " << event.motion.x << " " << event.motion.y << std::endl;
-				if ( updating ) {
-					destRectForFace.x = event.motion.x - 8;
-					destRectForFace.y = event.motion.y - 8;
+				if ( updating && ( timeToShoot <= 0 ) ) {
+					position_t np = {event.motion.x - 8, event.motion.y - 8};
+					np = np / scaleFactor;
+					Particle particle( np, {10, -20}, {0, 10} ) ;
+					particle.ttl = 20;
+					particles.push_back( particle );
+					timeToShoot = 0.1;
 				}
 				break;
 			}
@@ -107,8 +143,20 @@ int main( ) { // int argc, char **argv ) {
 
 		// fizyka
 
-		//     todo
+		if ( timeToSpawnParicle <= 0 ) {
+			position_t p = {uniform_dist_x(randomEngine),uniform_dist_y(randomEngine)};
+			p = p / scaleFactor;
+			Particle particle( p, {-10, 0}, {0, 0} ) ;
+			particle.ttl = 15;
+			particles.push_back( particle );
+			timeToSpawnParicle = 0.3;
+		}
 
+		calculateParticles( particles, dt ) ;
+		if ( timeToShoot > 0 )timeToShoot -= dt.count();
+		if ( timeToSpawnParicle > 0 ) timeToSpawnParicle -= dt.count();
+
+		
 		// grafika
 
 
@@ -116,12 +164,21 @@ int main( ) { // int argc, char **argv ) {
 		SDL_RenderClear( renderer.get() );
 
 		SDL_SetRenderDrawColor( renderer.get(), 255, 0, 0, 255 );
-		SDL_RenderDrawPoint( renderer.get(), 10, 10 );
-		SDL_RenderCopyEx( renderer.get(), particle_tex.get(), NULL,  &destRectForFace,  destRectForFace.x,  NULL, SDL_FLIP_NONE );
-
+		//SDL_RenderDrawPoint( renderer.get(), 10, 10 );
+		for ( auto & p : particles ) {
+			auto pos = p.position * scaleFactor;
+			SDL_Rect destRectForFace = {
+				.x = pos[0],
+				.y = pos[1],
+				.w = 16,
+				.h = 16
+			};
+			SDL_RenderCopyEx( renderer.get(), particle_tex.get(), NULL,  &destRectForFace,  destRectForFace.x,  NULL, SDL_FLIP_NONE );
+		}
 		SDL_RenderPresent( renderer.get() );
-		
-		SDL_Delay( std::chrono::duration<double, std::milli>( dt ).count() );
+
+		std::this_thread::sleep_until( prevTime + dt );
+		prevTime = std::chrono::high_resolution_clock::now();
 	}
 	return 0;
 }
