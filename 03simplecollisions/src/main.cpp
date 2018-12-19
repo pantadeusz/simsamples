d4ba3137734f4a11ec3b2ef7e81e75026ffa6a36
pantadeusz <none@none.com>
2018-12-19 13:24:42 +0100

    some nice animation

--- 03simplecollisions/src/main.cpp
+++ 03simplecollisions/src/main.cpp
@@ -67,148 +67,74 @@ std::shared_ptr<SDL_Renderer> init_renderer( std::shared_ptr<SDL_Window> window 
 	return renderer;
 }
 
+std::shared_ptr<SDL_Texture> load_texture( const std::shared_ptr<SDL_Renderer> renderer, const std::string fname ) {
+	SDL_Surface *bmp = SDL_LoadBMP( fname.c_str() );
+	if ( bmp == nullptr ) throw std::runtime_error( SDL_GetError() );
+	std::shared_ptr<SDL_Surface> bitmap ( bmp, []( SDL_Surface * ptr ) {
+		SDL_FreeSurface( ptr );
+	} );
 
-
-using vec_t = std::array<double, 2>;
-using ball_t = std::pair<vec_t, vec_t>;
-
-void draw_ball(const ball_t &ball,const double r, std::shared_ptr<SDL_Renderer> renderer) {
-
-	for (double a = -M_PI; a < M_PI; a+= 0.01) {
-                SDL_SetRenderDrawColor(renderer.get(), 255.0*(a/(M_PI))*(a/(M_PI)), 0, 255, SDL_ALPHA_OPAQUE);
-				SDL_RenderDrawLine(renderer.get(),
-                ball.first[0],
-                ball.first[1],
-                ball.first[0]+r*::sin(a),
-                ball.first[1]+r*::cos(a));
-	}
+	SDL_Texture *tex = SDL_CreateTextureFromSurface( renderer.get(), bitmap.get() );
+	if ( tex == nullptr ) throw std::runtime_error( SDL_GetError() );
+	std::shared_ptr<SDL_Texture> texture ( tex, []( SDL_Texture * ptr ) {
+		SDL_DestroyTexture( ptr );
+	} );
+	return texture;
 }
 
-vec_t operator-(const vec_t &a, const vec_t &b) {
-	return {a[0]-b[0],a[1]-b[1]};
-}
-vec_t operator+(const vec_t &a, const vec_t &b) {
-	return {a[0]+b[0],a[1]+b[1]};
-}
-vec_t operator*(const vec_t &a, const vec_t &b) {
-	return {a[0]*b[0],a[1]*b[1]};
-}
-vec_t operator*(const vec_t &a, const double b) {
-	return {a[0]*b,a[1]*b};
-}
-vec_t operator/(const vec_t &a, const double b) {
-	return {a[0]/b,a[1]/b};
-}
-double length(const vec_t &d_) {
-	auto d = d_*d_;
-	return std::sqrt(d[0]+d[1]);
+std::shared_ptr<SDL_Texture> load_png_texture( const std::shared_ptr<SDL_Renderer> renderer, const std::string fname ) {
+	std::vector<unsigned char> image;
+  	unsigned width, height;
+  	unsigned error = lodepng::decode(image, width, height, fname); // load png image to vector (image)
+	if(error) throw std::runtime_error( lodepng_error_text(error));
+
+	SDL_Texture * tex = SDL_CreateTexture( renderer.get(), SDL_PIXELFORMAT_ABGR8888, SDL_TEXTUREACCESS_STATIC, width, height );
+	if ( tex == nullptr ) throw std::runtime_error( SDL_GetError() );
+	std::shared_ptr<SDL_Texture> texture ( tex, []( SDL_Texture * ptr ) {SDL_DestroyTexture( ptr );} );
+	SDL_SetTextureBlendMode( texture.get(), SDL_BLENDMODE_BLEND ); // obslugujemy polprzezroczystosc
+	SDL_UpdateTexture( texture.get(), NULL, image.data(), width * sizeof( uint32_t ) );
+	return texture;
 }
 
+std::shared_ptr<SDL_Texture> create_texture( const std::shared_ptr<SDL_Renderer> renderer, const int w, const int h ) {
+	SDL_Texture * tex = SDL_CreateTexture( renderer.get(), SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STATIC, w, h );
+	if ( tex == nullptr ) throw std::runtime_error( SDL_GetError() );
+	std::shared_ptr<SDL_Texture> texture ( tex, []( SDL_Texture * ptr ) {SDL_DestroyTexture( ptr );} );
+	return texture;
+}
 
 int main( ) { // int argc, char **argv ) {
 
 	auto window = init_window();
 	auto renderer = init_renderer( window );
 
+	// we will be rendering data onto this:
+	auto game_texture_1 = load_texture( renderer, "data/face.bmp" );
+	auto game_texture = load_png_texture( renderer, "data/img.png" );
+	auto moving_point_texture = create_texture( renderer, 320, 200 );
+
+	SDL_SetTextureBlendMode( moving_point_texture.get(), SDL_BLENDMODE_BLEND );
+
 	std::random_device rd;
     std::mt19937 mt(rd());
     std::uniform_real_distribution<double> dist(1.0, 10.0);
 
-	std::vector < ball_t > balls = {{{20, 100},{0,0}},{{300, 100},{-100,0}},{{150, 150},{0,0}},{{150, 50},{0,0}}};
-
-	double dt = 0.01;
 	for ( bool game_active = true ; game_active; ) {
-		// events
 		SDL_Event event;
 		while ( SDL_PollEvent( &event ) ) {
-			switch (event.type) {
-				case SDL_QUIT: game_active = false; break;
-				case SDL_MOUSEMOTION: 
-					balls.at(0).second[0] = (balls.at(0).second[0] + 100*(event.motion.x - balls.at(0).first[0]))/2.0;
-					balls.at(0).second[1] = (balls.at(0).second[1] + 100*(event.motion.y - balls.at(0).first[1]))/2.0;
-
-					balls.at(0).first[0] = event.motion.x;
-					balls.at(0).first[1] = event.motion.y;
-				break;
-			}
+			if ( event.type == SDL_QUIT ) game_active = false;
 		}
-		// physics
 
-		// calculate collisions
-		std::vector<ball_t> new_balls;
-		for (const auto &active : balls) {
-			auto active_n = active;
-			for (const auto &test : balls) {
-				if (&test != &active) {
-					vec_t a_p,a_v,t_p,t_v;
-					std::tie(a_p,a_v) = active;
-					std::tie(t_p,t_v) = test;
-
-					double l = length(a_p-t_p);
-					if (l < 20.0) {
-						if (l < 10.0) l = 8; // no infinite movements
-						auto v = (t_p - a_p)/l; // 1
-						
-						active_n.second = active_n.second + t_v;
-						//std::cout << "collision " << a_v[0] << " " << a_v[1] << "\n";
-					}
-				}
-			}
-			new_balls.push_back(active_n);
-		}
-		balls = new_balls;
-		// move them away from each other
-		for (auto &active : balls) {
-			for (const auto &test : balls) {
-				if (&test != &active) {
-					vec_t a_p,a_v,t_p,t_v;
-					std::tie(a_p,a_v) = active;
-					std::tie(t_p,t_v) = test;
-
-					double l = length(a_p-t_p);
-					if (l < 20.0) {
-						auto v = (t_p - a_p)/l; // 1
-						a_p = t_p - v * 22;
-						std::cout << "collision " << a_v[0] << " " << a_v[1] << "\n";
-					}
-					active = {a_p,a_v};
-				}
-			}
-			if (active.first[0] < 0) {
-				active.first[0] = 0;
-				active.second[0] *= -1;
-			}
-			if (active.first[0] > 319) {
-				active.first[0] = 319;
-				active.second[0] *= -1;
-			}
-			if (active.first[1] < 0) {
-				active.first[1] = 0;
-				active.second[1] *= -1;
-			}
-			if (active.first[1] > 199) {
-				active.first[1] = 199;
-				active.second[1] *= -1;
-			}
-		}
-		// apply accel
-		for (auto &b : balls) {
-			if (&(balls[0]) == &b) {
-			} else {
-				b.first[0] = b.first[0] + b.second[0]*dt;
-				b.first[1] = b.first[1] + b.second[1]*dt;
-			}
-				b.second[0] = b.second[0] - b.second[0]*1*dt;
-				b.second[1] = b.second[1] - b.second[1]*1*dt;
-		}
-		// video
-        SDL_SetRenderDrawColor(renderer.get(), 0, 0, 0, SDL_ALPHA_OPAQUE);
 		SDL_RenderClear( renderer.get() );
-		for (auto &b:balls) draw_ball(b,10.5,renderer);
 
 
+		SDL_RenderClear( renderer.get() );
+		SDL_Rect dstrect = {0 + dist(mt)-5,0 + dist(mt)-5,320,200};
+		SDL_RenderCopy( renderer.get(), game_texture_1.get(), NULL, &dstrect );
+		SDL_RenderCopy( renderer.get(), game_texture.get(), NULL, NULL );
+		SDL_RenderCopy( renderer.get(), moving_point_texture.get(), NULL, NULL );
 		SDL_RenderPresent( renderer.get() );
-		SDL_Delay( 1000.0*dt );
+		SDL_Delay( 10 );
 	}
 	return 0;
 }
