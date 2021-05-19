9a58facaae3154200a7a956a016273235804bba5
pantadeusz <pantadeusz+git@pjwstk.edu.pl>
2021-05-19 12:27:01 +0200

    finished example with simple physics

--- bullethell/src/main.cpp
+++ bullethell/src/main.cpp
@@ -27,7 +27,8 @@
 #include <vector>
 
 
-std::ostream &operator<<(std::ostream &o, const std::array<double,2> &a) {
+std::ostream& operator<<(std::ostream& o, const std::array<double, 2>& a)
+{
     o << "[" << a[0] << "," << a[1] << "]";
     return o;
 }
@@ -44,13 +45,16 @@ public:
     std::array<double, 2> position;
     std::array<double, 2> velocity;
     std::array<double, 2> acceleration;
+    double friction;
 
-    void update(double dt_f, std::function<void(physical_c *, std::array<double, 2> &pos, std::array<double, 2> &vel)> callback_f)
+    void update(double dt_f, std::function<void(physical_c*, std::array<double, 2>& pos, std::array<double, 2>& vel)> callback_f)
     {
         using namespace tp::operators;
-        auto new_position = position + velocity * dt_f + acceleration * dt_f * dt_f * 0.5;
-		auto new_velocity = velocity + acceleration * dt_f;
-        callback_f(this,new_position, new_velocity);
+        // apply friction:
+        auto new_acceleration = acceleration - velocity*length(velocity)*friction;
+        auto new_velocity = velocity + new_acceleration * dt_f;
+        auto new_position = position + new_velocity * dt_f + new_acceleration * dt_f * dt_f * 0.5;
+        callback_f(this, new_position, new_velocity);
     }
 };
 
@@ -61,20 +65,18 @@ public:
 
     player_c()
     {
-        position = {160, 80};
+        position = {10, 10};
         velocity = {0, 0};
+        friction = 0.03;
+        acceleration = {0,0};
     }
 
-/**
+    /**
  * applies and clears intentions
  * */
-    void apply_intent() {
-       // velocity = {0, 0};//50};
-        acceleration = {0,0};
-//        if (intentions.count("right")) velocity[0] += 10;
-//        if (intentions.count("left")) velocity[0] += -10;
-//        if (intentions.count("up")) velocity[1] += -10;
-//        if (intentions.count("down")) velocity[1] += +10;
+    void apply_intent()
+    {
+        acceleration = {0, 30};
         if (intentions.count("right")) acceleration[0] += 100;
         if (intentions.count("left")) acceleration[0] += -100;
         if (intentions.count("up")) acceleration[1] += -100;
@@ -104,7 +106,7 @@ int main(int, char**)
         });
 
     SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "0");
-    SDL_RenderSetLogicalSize(renderer_p.get(), 320, 180);
+    SDL_RenderSetLogicalSize(renderer_p.get(), 640, 360);
 
     shared_ptr<SDL_Texture> tex_p(IMG_LoadTexture(renderer_p.get(), "data/player.png"),
         [](auto* tex) { SDL_DestroyTexture(tex); });
@@ -127,39 +129,35 @@ int main(int, char**)
         if (kbdstate[SDL_SCANCODE_LEFT]) player.intentions["left"] = 1;
         if (kbdstate[SDL_SCANCODE_UP]) player.intentions["up"] = 1;
         if (kbdstate[SDL_SCANCODE_DOWN]) player.intentions["down"] = 1;
-        //for (auto [k,v] : player.intentions) {
-        //    std::cout << "[" << k << ":" << v << "] ";
-        //}
-        //std::cout << std::endl;
-        //    if (kbdstate[SDL_SCANCODE_RIGHT])
-
 
         /// fizyka
         double dt_f = dt.count() / 1000.0;
         player.apply_intent();
-        player.update(dt_f, [&](auto p, auto pos, auto vel){
-            //if (pos[1] < 150) {
+        player.update(dt_f, [&](auto p, auto pos, auto vel) {
+            if (pos[1] < 30) {
                 p->position = pos;
                 p->velocity = vel;
-            /*} else {
-                p->velocity = {vel[0],0};
+                p->friction = 0.2;
+            } else {
+                p->velocity = {(vel[0]*vel[0]>2.2)?vel[0]:0.0, 0};
                 p->position[0] = pos[0];
-            }*/
+                p->friction = 0.3;
+            }
         });
         /// grafika
         SDL_SetRenderDrawColor(renderer_p.get(), 0, 100, 20, 255);
         SDL_RenderClear(renderer_p.get());
         SDL_SetRenderDrawColor(renderer_p.get(), 255, 100, 200, 255);
         SDL_RenderCopy(renderer_p.get(), tex_p.get(), NULL, NULL);
-        draw_o(renderer_p, player.position, tex_p, 16, 16, 30);
+        draw_o(renderer_p, player.position*10.0, tex_p, 16, 16, player.position[0]*36+player.position[1]*5);
         //draw_o(renderer_p.get(),{50,20},tex_p.get(),16,16,30);
         SDL_RenderPresent(renderer_p.get());
 
         this_thread::sleep_until(current_time = current_time + dt);
 
         steady_clock::time_point frame_end = steady_clock::now();
-        std::cout << "frame time: " << (frame_end - frame_start).count() << std::endl;
-
+        
+       // std::cout << "frame time: " << (frame_end - frame_start).count() << std::endl;
     }
     SDL_Quit();
     return 0;
