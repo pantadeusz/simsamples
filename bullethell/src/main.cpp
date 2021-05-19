08b301f61eb6d9f53649e35cb7c3e82be717aa51
pantadeusz <pantadeusz+git@pjwstk.edu.pl>
2021-05-19 09:44:04 +0200

    po wykladzie

--- bullethell/src/main.cpp
+++ bullethell/src/main.cpp
@@ -17,6 +17,7 @@
 #include <functional>
 #include <iomanip>
 #include <iostream>
+#include <map>
 #include <memory>
 #include <set>
 #include <stdexcept>
@@ -25,16 +26,57 @@
 #include <tuple>
 #include <vector>
 
-void draw_o(SDL_Renderer* r, std::array<double, 2> p, SDL_Texture* tex, double w, double h, double a)
+void draw_o(std::shared_ptr<SDL_Renderer> r, std::array<double, 2> p, std::shared_ptr<SDL_Texture> tex, double w, double h, double a)
 {
     SDL_Rect dst_rect = {(int)(p[0] - w / 2), (int)(p[1] - h / 2), (int)w, (int)h};
-    SDL_RenderCopyEx(&*r, &*tex, NULL, &dst_rect, a, NULL, SDL_RendererFlip::SDL_FLIP_NONE);
+    SDL_RenderCopyEx(r.get(), tex.get(), NULL, &dst_rect, a, NULL, SDL_RendererFlip::SDL_FLIP_NONE);
 }
+
+class physical_c
+{
+public:
+    std::array<double, 2> position;
+    std::array<double, 2> velocity;
+    std::array<double, 2> acceleration;
+
+    void update(double dt_f, std::function<void(physical_c *, std::array<double, 2> &pos, std::array<double, 2> &vel)> callback_f)
+    {
+        using namespace tp::operators;
+        std::cout << "acc: " << acceleration[0] << "," << acceleration[1] << " ||| " << velocity[0] << "," << velocity[1] << std::endl;
+        auto new_position = position + velocity * dt_f + velocity * acceleration * dt_f * dt_f * 0.5;
+        auto new_velocity = (velocity + acceleration * dt_f) * 0.94;
+        callback_f(this,new_position, new_velocity);
+    }
+};
+
+class player_c : public physical_c
+{
+public:
+    std::map<std::string, int> intentions;
+
+    player_c()
+    {
+        position = {0, 80};
+        velocity = {50, -4};
+    }
+
+/**
+ * applies and clears intentions
+ * */
+    void apply_intent() {
+        acceleration = {0, 50};
+        if (intentions.count("left")) acceleration[0] += -100;
+        if (intentions.count("right")) acceleration[0] += 100;
+
+        intentions.clear();
+    }
+};
 
 int main(int, char**)
 {
     using namespace std;
     using namespace std::chrono;
+    using namespace tp::operators;
 
     SDL_Init(SDL_INIT_EVERYTHING);
     IMG_Init(IMG_INIT_PNG);
@@ -55,70 +97,50 @@ int main(int, char**)
     shared_ptr<SDL_Texture> tex_p(IMG_LoadTexture(&*renderer_p, "data/player.png"),
         [](auto* tex) { SDL_DestroyTexture(tex); });
 
+
+    player_c player;
+
     milliseconds dt(15);
     steady_clock::time_point current_time = steady_clock::now(); // remember current time
+
     for (bool game_active = true; game_active;) {
         SDL_Event event;
         while (SDL_PollEvent(&event)) { // check if there are some events
             if (event.type == SDL_QUIT)
                 game_active = false;
         }
-        //auto kbdstate = SDL_GetKeyboardState(NULL);
-        //    if (kbdstate[SDL_SCANCODE_LEFT])
+        auto kbdstate = SDL_GetKeyboardState(NULL);
+        if (kbdstate[SDL_SCANCODE_LEFT]) player.intentions["left"] = 1;
+        if (kbdstate[SDL_SCANCODE_RIGHT]) player.intentions["right"] = 1;
+        //for (auto [k,v] : player.intentions) {
+        //    std::cout << "[" << k << ":" << v << "] ";
+        //}
+        //std::cout << std::endl;
         //    if (kbdstate[SDL_SCANCODE_RIGHT])
 
+
+        /// fizyka
+        double dt_f = dt.count() / 1000.0;
+        player.apply_intent();
+        player.update(dt_f, [&](auto p, auto pos, auto vel){
+            if (pos[1] < 150) {
+                p->position = pos;
+                p->velocity = vel;
+            } else {
+                p->velocity = {vel[0],0};
+                p->position[0] = pos[0];
+            }
+        });
+        /// grafika
         SDL_SetRenderDrawColor(&*renderer_p, 0, 100, 20, 255);
         SDL_RenderClear(&*renderer_p);
         SDL_SetRenderDrawColor(&*renderer_p, 255, 100, 200, 255);
         SDL_RenderCopy(&*renderer_p, &*tex_p, NULL, NULL);
-        draw_o(&*renderer_p,{10,20},&*tex_p,16,16,0);
-        draw_o(&*renderer_p,{50,20},&*tex_p,16,16,30);
+        draw_o(renderer_p, player.position, tex_p, 16, 16, 30);
+        //draw_o(&*renderer_p,{50,20},&*tex_p,16,16,30);
         SDL_RenderPresent(&*renderer_p);
         this_thread::sleep_until(current_time = current_time + dt);
     }
     SDL_Quit();
     return 0;
 }
-
-
-/**
- * see how this compiles
- * */
-#ifdef DO_THE_GODBOLT_DEMO
-#include <array>
-
-template <typename T1, typename T2>
-inline T1 operator+(const T1& a, const T2& b)
-{
-    T1 ret;
-    for (unsigned i = 0; i < ((a.size() > b.size()) ? b.size() : a.size()); i++)
-        ret[i] = a[i] + b[i];
-    return ret;
-}
-
-template <typename T1, typename T2>
-inline T1 operator-(const T1& a, const T2& b)
-{
-    T1 ret;
-    for (unsigned i = 0; i < ((a.size() > b.size()) ? b.size() : a.size()); i++)
-        ret[i] = a[i] - b[i];
-    return ret;
-}
-
-template <typename T>
-inline double length(const T& a)
-{
-    double ret = 0.0;
-    for (unsigned i = 0; i < a.size(); i++)
-        ret += a[i] * a[i];
-    return (ret == 0) ? 0.0 : std::sqrt(ret);
-}
-
-int main(int argc)
-{
-    std::array<double, 2> p1 = {(double)argc, 1.0};
-    std::array<double, 2> p2 = {2.0, (double)argc};
-    auto r = p1 + p2;
-    return r[0] + r[1];
-}
-#endif
--- bullethell/src/vectors.hpp
+++ bullethell/src/vectors.hpp
@@ -1,6 +1,6 @@
 #ifndef ___VECTORS_TP_PJATK_HPP___
 #define ___VECTORS_TP_PJATK_HPP___
-
+#include <array>
 #include <cmath>
 
 namespace tp::operators {
@@ -9,6 +9,18 @@ template<typename T1,typename T2>
 inline T1 operator+(const T1& a, const T2& b) { 
     T1 ret = a;
     for (unsigned i = 0; i < a.size();i++) ret[i] += b[i];
+    return ret;
+}
+
+inline std::array<double,2> operator*(const std::array<double,2>& a, const std::array<double,2>& b) { 
+    std::array<double,2> ret = a;
+    for (unsigned i = 0; i < a.size();i++) ret[i] *= b[i];
+    return ret;
+}
+
+inline std::array<double,2> operator*(const std::array<double,2>& a, double b) { 
+    std::array<double,2> ret = a;
+    for (unsigned i = 0; i < a.size();i++) ret[i] *= b;
     return ret;
 }
 
